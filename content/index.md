c9ba3b6694f23ae50b423e7cc67f685e46a8a44d
ajkdrag <altukhan43@gmail.com>
2024-10-05 11:46:23 +0530

    Quartz sync: Oct 5, 2024, 11:46 AM

---
+++ content/index.md
@@ -0,0 +1,7 @@
+---
+title: Red Buffs
+draft: false
+tags:
+---
+
+This is my index page. Welcome to you all! Hope you are having a nice day :)
--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,8 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/ajkdrag",
+      LinkedIn: "https://www.linkedin.com/in/altamash-khan-7183681b8",
     },
   }),
 }
